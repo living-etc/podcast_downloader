9a33aed75b5e15515f856b9910c215910d67b183
Chris Wilson <chris.wilson@outlook.com>
2017-01-04 18:44:57 +0000

    Add downloaders

---
+++ pseudopod-downloader/escape_artists_downloader.rb
@@ -0,0 +1,44 @@
+require 'nokogiri'
+require 'open-uri'
+require 'net/http'
+require 'uri'
+
+def parse_sitemap(sitemap)
+  Nokogiri::XML(open(sitemap)).tap do |doc|
+    doc.xpath('//xmlns:loc').map(&:text).each { |links| yield links }
+  end
+end
+
+def download(link)
+  if link.first
+    uri = URI(link.first.attributes['href'])
+
+    begin
+      puts "Downloading #{uri.to_s}"
+      File.open("/Users/chriswilson/Desktop/pseudopod/#{File.basename(uri.path)}", 'wb') do |file|
+        open(uri.to_s, 'rb') do |download|
+          file.write(download.read)
+        end
+      end
+    rescue Exception => e
+      puts "    Retrying #{uri.to_s}"
+      retry
+    end
+  end
+end
+
+def all_links_from(root_sitemap)
+  parse_sitemap(root_sitemap) do |sitemap|
+    parse_sitemap(sitemap) do |post|
+      if post.match(/\d{4}\/\d{2}\/\d{2}/)
+        Nokogiri::HTML(open(post.strip)).tap do |doc|
+          doc.xpath('//a[contains(@class, "podpress_downloadimglink podpress_downloadimglink_audio_mp3")]').tap do |link|
+            download(link)
+          end
+        end
+      end
+    end
+  end
+end
+
+all_links_from('http://pseudopod.org/sitemap.xml')
